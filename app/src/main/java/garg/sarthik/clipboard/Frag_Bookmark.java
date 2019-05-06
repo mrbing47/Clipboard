873663436d96f47285ad9aba58d5018c7f2c2f63
Sarthik <sarthikgarg01@gmail.com>
2019-05-07 00:40:23 +0530

    Stable code, I guess

--- app/src/main/java/garg/sarthik/clipboard/Frag_Bookmark.java
+++ app/src/main/java/garg/sarthik/clipboard/Frag_Bookmark.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,23 +28,29 @@ public class Frag_Bookmark extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        clipList = ClipApplication.getClipDb().getClipDao().getBookmarked();
-        View view;
-        if (clipList.isEmpty())
-            view = inflater.inflate(R.layout.layout_nobookmark, container, false);
-        else {
-            view = inflater.inflate(R.layout.layout_rview, container, false);
-            rvClipBoard = view.findViewById(R.id.rvClipBoard);
-            callAdapter();
-        }
+//        clipList = ClipApplication.getClipDb().getClipDao().getBookmarked();
+//        View view;
+//        if (clipList.isEmpty())
+//            view = inflater.inflate(R.layout.layout_nobookmark, container, false);
+//        else {
+//            view = inflater.inflate(R.layout.layout_rview, container, false);
+//            rvClipBoard = view.findViewById(R.id.rvClipBoard);
+//            callAdapter();
+//        }
+        View view = inflater.inflate(R.layout.layout_nobookmark, container, false);
+        rvClipBoard = view.findViewById(R.id.rvClipBoardBookmark);
+
+        callAdapter();
         return view;
     }
 
     private void callAdapter() {
 
         clipList = ClipApplication.getClipDb().getClipDao().getBookmarked();
+        Log.e("TAG_ClipBookmark", "callAdapter: \n\n" + clipList.size());
+        if (!clipList.isEmpty()) {
 
-        if(!clipList.isEmpty()) {
+            rvClipBoard.setVisibility(View.VISIBLE);
 
             if (Statics.layout.equals(Statics.gridView))
                 rvClipBoard.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
@@ -57,7 +64,8 @@ public class Frag_Bookmark extends Fragment {
                 rvClipBoard.addItemDecoration(new Statics.SpaceItemDecoration(clipList, getContext()));
                 isBordered = true;
             }
-        }
+        } else
+            rvClipBoard.setVisibility(View.GONE);
     }
 
     public void update() {
--- app/src/main/java/garg/sarthik/clipboard/Frag_Clip.java
+++ app/src/main/java/garg/sarthik/clipboard/Frag_Clip.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,23 +28,21 @@ public class Frag_Clip extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        clipList = ClipApplication.getClipDb().getClipDao().getAll();
-        View view;
-        if (clipList.isEmpty())
-            view = inflater.inflate(R.layout.layout_noclip, container, false);
-        else {
-            view = inflater.inflate(R.layout.layout_rview, container, false);
-            rvClipBoard = view.findViewById(R.id.rvClipBoard);
-            callAdapter();
-        }
+        View view = inflater.inflate(R.layout.layout_noclip, container, false);
+        rvClipBoard = view.findViewById(R.id.rvClipBoardAll);
+
+        callAdapter();
         return view;
     }
 
     private void callAdapter() {
 
         clipList = ClipApplication.getClipDb().getClipDao().getAll();
-
+        Log.e("TAG_ClipAll", "callAdapter: \n\n" + clipList.size());
         if (!clipList.isEmpty()) {
+
+            rvClipBoard.setVisibility(View.VISIBLE);
+
             if (Statics.layout.equals(Statics.gridView))
                 rvClipBoard.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
             else
@@ -57,7 +56,8 @@ public class Frag_Clip extends Fragment {
                 rvClipBoard.addItemDecoration(new Statics.SpaceItemDecoration(clipList, getContext()));
                 isBordered = true;
             }
-        }
+        } else
+            rvClipBoard.setVisibility(View.GONE);
     }
 
     void update() {
